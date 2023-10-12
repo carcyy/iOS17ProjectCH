950b9cda04e2c0b362c4485b72bdafbd6ebbeedd
Carson H <68257194+carcyy@users.noreply.github.com>
2023-10-12 14:20:37 -0600

    Initial Commit

---
+++ iOS17Project/iOS17ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS17ProjectApp.swift
+//  iOS17Project
+//
+//  Created by CUBS Customer on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS17ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
