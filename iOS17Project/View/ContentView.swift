77c5830415badbce83dcb0e5f325a458113b7c46
Carson H <68257194+carcyy@users.noreply.github.com>
2023-10-20 19:07:41 -0600

    figma progress
    square button to expand animation

--- iOS17Project/View/ContentView.swift
+++ iOS17Project/View/ContentView.swift
@@ -100,4 +100,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
--- iOS17Project/View/HomeViewOther.swift
+++ iOS17Project/View/HomeViewOther.swift
@@ -1,28 +1,27 @@
 //
-//  HomeView.swift
+//  HomeViewOther.swift
 //  iOS17Project
 //
-//  Created by CUBS Customer on 10/19/23.
+//  Created by CUBS Customer on 10/20/23.
 //
 
 import SwiftUI
 
-struct HomeView: View {
+struct HomeViewOther: View {
     
     @State private var isLiked: Bool = UserDefaults.standard.bool(forKey: "isLiked")
-    @State private var isExpanded: Bool = false
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 35)
-                .foregroundColor(Color(hue: 0.1, saturation: 0.3, brightness: 0.8))
+                .foregroundColor(Color(hue: 0.7, saturation: 0.2, brightness: 0.1))
                 .frame(minWidth: 0, maxWidth: 500, minHeight: 0, maxHeight: 700)
                 .padding(25)
             
             RoundedRectangle(cornerRadius: 35)
                 .foregroundColor(Color.white)
-                .frame(maxWidth: isExpanded ? 300 : 300, maxHeight: isExpanded ? 500 : 200)
-                .padding([.top], isExpanded ? 0 : 400)
+                .frame(maxWidth: 300, maxHeight: 200)
+                .padding([.top], 400)
                 .opacity(0.5)
             
             Button(action: {
@@ -30,25 +29,12 @@ struct HomeView: View {
                 UserDefaults.standard.set(isLiked, forKey: "isLiked")
             }) {
                 Image(systemName: isLiked ? "heart.fill" : "heart")
-                    .foregroundColor(isLiked ? .red : .brown)
+                    .foregroundColor(isLiked ? .red : .black)
                     .scaledToFit()
                     .scaleEffect(2)
             }
             .padding([.top], 300)
             .padding([.leading], 200)
-            
-            Button(action: {
-                withAnimation {
-                    isExpanded.toggle()
-                }
-            }) {
-                Image(systemName: "square")
-                    .foregroundColor(isExpanded ? .red : .black)
-                    .scaledToFit()
-                    .scaleEffect(2)
-            }
-            .padding([.top], 300)
-            .padding([.leading], 100)
         }
     }
 }
